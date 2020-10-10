d5295c3473f3f4a47d761c44f0ddad6dd82c975e
Zabielski <michalx.zabielski@intel.com>
2020-10-10 13:07:46 +0100

    fix mess with packages move all models to CarRental.Models

---
+++ src/CarRental.Models/Extensions/CarExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Models.Extensions
+{
+    public static class CarExtensions
+    {
+        public static bool IsReserved(this Car car) // car.IsReserved IsReserved(car)
+        {
+            if (car.ReservationHistory == null)
+            {
+                return false;
+            }
+
+            foreach (var reservation in car.ReservationHistory)
+            {
+                if (reservation.EndDate > DateTime.Now && reservation.StartDate <= DateTime.Now)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
