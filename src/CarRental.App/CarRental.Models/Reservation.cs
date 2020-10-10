f7bf925c35ba4f659b49069e604ae347831971a7
= <przemek.ciszak@gmail.com>
2020-10-10 11:10:28 +0200

    Add car related models

---
+++ src/CarRental.App/CarRental.Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Models
+{
+    public class Reservation
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ src/CarRental.App/CarRental.Models/Segment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Models
+{
+    public enum Segment
+    {
+        Unknown,
+        Family,
+        Sport,
+        Premium
+    }
+}
